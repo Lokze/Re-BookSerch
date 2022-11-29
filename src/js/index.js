8c8c9113ac8fedee9fcc640de0cbd5f312c79f6a
Lorenzo Loi <51636003+Lokze@users.noreply.github.com>
2022-11-29 22:03:35 +0100

    end good to go

--- src/js/index.js
+++ src/js/index.js
@@ -9,19 +9,60 @@ submit.onclick= async function bookSerch() {
     
     document.getElementById("output").innerHTML+="<div id='loader'></div>";
     
-    const response= await axios.get ("https://openlibrary.org/subjects/"+document.getElementById("genere").value+".json?details=true");
-    if(response.status!= 200){
-        throw new Error('Request faild with status ${'+response.status+'}');
-    }
+    let response;
+
+         response= await axios.get ("https://openlibrary.org/subjects/"+document.getElementById("genere").value+".json?details=true")
+         .catch(function(error){
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+                
+            }
+            else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(error.request);
+              }
+              else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+              }
+              console.log(error.config);
+              document.getElementById("output").removeChild(document.getElementById('loader'));
+         });
+   
     var data = response.data;
 
     document.getElementById("output").removeChild(document.getElementById('loader'));
     
     for(var i=0; i<data.works.length; i++){
-        const responseKey = await axios.get("https://openlibrary.org"+data.works[i].key+".json");
-        if(response.status!= 200){
-            throw new Error('Request faild with status {'+response.status+'}');
-        }
+        const responseKey = await axios.get("https://openlibrary.org"+data.works[i].key+".json") .catch(function(error){
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+                
+            }
+            else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(error.request);
+              }
+              else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+              }
+              console.log(error.config);
+              document.getElementById("output").removeChild(document.getElementById('loader'));
+         });
+
         var dataKey = responseKey.data;
         
         document.getElementById("output").innerHTML+="<div id='container'>"
