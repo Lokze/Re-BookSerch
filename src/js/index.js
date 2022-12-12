f8280e73603839ae3da544942938d000f035cbb8
Lorenzo Loi <51636003+Lokze@users.noreply.github.com>
2022-12-12 18:18:47 +0100

    correction

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,16 @@ import lodash from 'lodash';
 import '../css/books.css';
 
 let submit = document.getElementById("submit");
+let output= document.getElementById("output");
 
 submit.onclick= async function bookSerch() {
-    document.getElementById("output").innerHTML="";
+    output.innerHTML="";
     
-    document.getElementById("output").innerHTML+="<div id='loader'></div>";
+    output.innerHTML+="<div id='loader'></div>";
     
     let response;
 
-         response= await axios.get ("https://openlibrary.org/subjects/"+document.getElementById("genere").value+".json?details=true")
+         response= await axios.get (`https://openlibrary.org/subjects/${document.getElementById("genere").value}.json?details=true`)
          .catch(function(error){
             if (error.response) {
                 // The request was made and the server responded with a status code
@@ -32,15 +33,17 @@ submit.onclick= async function bookSerch() {
                 console.log('Error', error.message);
               }
               console.log(error.config);
-              document.getElementById("output").removeChild(document.getElementById('loader'));
+             output.removeChild(document.getElementById('loader'));
          });
    
     var data = response.data;
 
-    document.getElementById("output").removeChild(document.getElementById('loader'));
-    
-    for(var i=0; i<data.works.length; i++){
-        const responseKey = await axios.get("https://openlibrary.org"+data.works[i].key+".json") .catch(function(error){
+   output.removeChild(document.getElementById('loader'));
+
+   var bookWorks = lodash.get(data, 'works'); 
+   
+   bookWorks.forEach( async work => {
+    const responseKey = await axios.get(`https://openlibrary.org${work.key}.json`) .catch(function(error){
             if (error.response) {
                 // The request was made and the server responded with a status code
                 // that falls out of the range of 2xx
@@ -59,25 +62,37 @@ submit.onclick= async function bookSerch() {
                 console.log('Error', error.message);
               }
               console.log(error.config);
-              document.getElementById("output").removeChild(document.getElementById('loader'));
+             output.removeChild(document.getElementById('loader'));
          });
 
         var dataKey = responseKey.data;
         
-        document.getElementById("output").innerHTML+="<div id='container'>"
-        for(var e=0; e<data.works[i].authors.length; e++){
-            document.getElementById("output").innerHTML+="<p class='author'>"+lodash.get(data, ['works',i,'authors',e,'name'])+" </p>";
-        }
-        document.getElementById("output").innerHTML+="<a href='#anchor' class='books' onclick='showDesc("+i+");' id='bookTitle'>"+lodash.get(data, ['works',i,'title'])+"</a>";
-      
-        
-        if(typeof dataKey.description == 'string')
-        document.getElementById("output").innerHTML+="<p id='"+i+"' class='description' hidden>"+lodash.get(dataKey, ['description'])+"</p></div>";
-        else
-        document.getElementById("output").innerHTML+="<p id='"+i+"' class='description' hidden>"+lodash.get(dataKey, ['description','value'])+"</p></div>";
-        
-    }
+        output.innerHTML+="<div id='container'>"
+       
+        work.authors.forEach( author => {
 
-    
+          output.innerHTML+=`<p class='author'>${author.name} </p>`;
+          
+        });
+      
+       output.innerHTML+=`<a href='#anchor' class='books' onclick="showDesc('${work.key}')" id='bookTitle'>${work.title}</a>`;
+      
+
+       var desc;
+       if(typeof dataKey.description == 'string')
+       desc= lodash.get(dataKey, ['description']);
+        else
+       desc= lodash.get(dataKey, ['description','value']);
+
+       output.innerHTML+=`<p id='${work.key}' class='description' hidden>${desc}</p></div>`;
+   });
+       
+       /* if(typeof dataKey.description == 'string')
+       output.innerHTML+=`<p id='${work.key}' class='description' hidden>${lodash.get(dataKey, ['description'])}</p></div>`;
+        else
+       output.innerHTML+=`<p id='${work.key}' class='description' hidden>${lodash.get(dataKey, ['description','value'])}</p></div>`;
+        
+   });*/
+   
 }
 
